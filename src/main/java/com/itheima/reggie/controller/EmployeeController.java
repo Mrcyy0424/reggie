d83bb5b0777ece0daa3f438e7041aac35bfa69c6
Cyyun <10957898+cyy0424@user.noreply.gitee.com>
2023-05-03 14:59:44 +0800

    c-01

---
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.itheima.reggie.controller;
+
+
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Employee;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeController employeeController;
+
+
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+
+        return null;
+    }
+}
+
